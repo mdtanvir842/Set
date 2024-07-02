9ec773d310a20406faf9e080dc90760227cc4ae6
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-02 12:30:24 +0600

    Create set_06_first-last-element.cpp

---
+++ set_06_first-last-element.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    set<int>st={1,2,3,4,5,6,7,8,9,10};
+    cout<<*st.begin()<<endl;//first element
+    cout<<*++st.begin()<<endl;//second element
+    cout<<*--st.end()<<endl;//last element
+    //cout<<*++++st.begin()<<endl;//third element    
+    //cout<<*----st.end()<<endl;//before last element
+}
