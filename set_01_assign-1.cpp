565b7ca27f7855f9dee829af682a6f1e3c129123
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-02 10:56:55 +0600

    Create set_01_assign-1.cpp

---
+++ set_01_assign-1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    set<int>st={1,2,3,4,5,8,1,6,8,0,3};
+    for(auto u:st){
+        cout<<u<<endl;
+    }
+    //1.unique
+    //2.sort
+    //Both done automatically.
+}
